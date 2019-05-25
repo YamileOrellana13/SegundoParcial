46bf30f15e33c79262fdf845ada882542c3c4051
yamileandreaorellanasejas@gmail.com <yamileandreaorellanasejas@gmail.com>
2019-05-25 10:20:29 -0400

    Commit Xamarin Creada

--- YamileOrellanaParcial2/App/App/App.xaml.cs
+++ YamileOrellanaParcial2/App/App/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using App.Views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -11,7 +12,7 @@ namespace App
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NotePage();
         }
 
         protected override void OnStart()
---
+++ YamileOrellanaParcial2/App/App/Interfaces/InstanceLocator.cs
@@ -0,0 +1,23 @@
+﻿using App.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Interfaces
+{
+  
+    public class InstanceLocator
+    {
+        #region Properties
+        public MainViewModel Main { get; set; }
+        #endregion
+
+        #region Constructors
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+        #endregion
+    }
+
+}
